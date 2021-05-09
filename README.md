65c56264f6cb662588b6f8867153f5feda1140fa
gabrielnsd <gabrielnsdantas@gmail.com>
2021-05-09 18:00:10 -0300

    first commmit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Simpatibot
+
+Esta é a Simpatibot, uma bot para telegram que adiciona os luxos compartilhados no grupo à uma playlist colaborativa no Spotify.
+
+## Dependências
+
+- python 3.8  
+- python-telegram-bot
+
+## Contribuindo
+
+1. Faça um fork deste repositório
+
+2. Crie um branch para editar o que achar pertinente  
+    `git checkout -b nome-do-branch main`
+
+3. Faça seus commits
+
+4. Abra um PR após testar sua solução
+
+## Executando a Simpatibot
+
+1. Insira as chaves de API no arquivo .env (ver .env.example)
+
+2. Inicialize o ambiente virtual  
+   `$ source <venv>/bin/activate` (Para shell bash/zsh, para mais informações sobre venv e como executar em outras plataformas acesse: [venv](https://docs.python.org/3/library/venv.html))
+
+3. Inicie o bot executando o comando  
+   `python3 sript.py`
---
+++ script.py
@@ -0,0 +1,84 @@
+from telegram.ext import InlineQueryHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+import os
+import telegram
+import logging
+from dotenv import load_dotenv
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+
+
+load_dotenv()
+
+token = os.environ['TELEGRAM_BOT_APIKEY']
+bot = telegram.Bot(token=token)
+
+updater = Updater(token=token, use_context=True)
+
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Eu sou a SimpatiBot, manda o luxo aí")
+
+
+start_handler = CommandHandler('start', start)
+
+dispatcher.add_handler(start_handler)
+
+
+def echo(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text)
+
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+
+def caps(update, context):
+    print(context)
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+
+def dot(update, context):
+    text_dot = ' '.join(context.args) + '.'
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_dot)
+
+
+dot_handler = CommandHandler('dot', dot)
+dispatcher.add_handler(dot_handler)
+
+
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+
+updater.start_polling()
